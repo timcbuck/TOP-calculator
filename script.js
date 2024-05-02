d3082c22f9c9df68551ef8fd7be5c5e97b64d87e
timcbuck <timcbuck.94@gmail.com>
2024-05-02 11:29:43 +0930

    Fix display issue when number is a long decimal

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let firstNumber = 0;
 let secondNumber = 0;
 let result = 0;
 let operation = "";
+let isOperationClicked = false;
 
 
-function checkMaxNumberReached(val) {
-    if (val.length > 9) {
+function checkMaxNumberReached(result) {
+    if (result > 9999999999) {
         return "9999999999";
     } else {
-        return val;
+        return result;
     }
 }
 
@@ -16,15 +17,18 @@ function checkMaxNumberReached(val) {
 function updateDisplay(num) {
     let displayNumber = getDisplayNumber();
 
+    // Clear display number if operation clicked or only 0 displayed
+    if (displayNumber === "0" || isOperationClicked == true) {
+        displayNumber = "";
+        isOperationClicked = false;
+    }
+
+    // Check if number is greater than maximum display, if it is, prevent user from appending more numbers
     if (displayNumber.length > 9) {
         displayNumber = "9999999999"; // maximum display storage
         return;
     }
 
-    // Remove leading zero if starting at 0
-    if (displayNumber === "0") {
-        displayNumber = "";
-    } 
     // Append the new number to the end of the current number
     displayNumber = displayNumber + num;
     // Update the display number div text with the new number
@@ -45,50 +49,30 @@ function getDisplayNumber() {
 
 
 function add() {
-    /*
-    1. Get the number in the display and store it in the global variable
-    2. Set global operation variable to "add"
-    3. Clear display
-    */
    firstNumber = getDisplayNumber();
    operation = "add";
-   clearDisplay();
+   isOperationClicked = true;
 }
 
 
 function subtract() {
-    /*
-    1. Get the number in the display and store it in the global variable
-    2. Set global operation variable to "subtract"
-    3. Clear display
-    */
    firstNumber = getDisplayNumber();
    operation = "subtract";
-   clearDisplay();
+   isOperationClicked = true;
 }
 
 
 function divide() {
-    /*
-    1. Get the number in the display and store it in the global variable
-    2. Set global operation variable to "subtract"
-    3. Clear display
-    */
    firstNumber = getDisplayNumber();
    operation = "divide";
-   clearDisplay();
+   isOperationClicked = true;
 }
 
 
 function multiply() {
-    /*
-    1. Get the number in the display and store it in the global variable
-    2. Set global operation variable to "subtract"
-    3. Clear display
-    */
    firstNumber = getDisplayNumber();
    operation = "multiply";
-   clearDisplay();
+   isOperationClicked = true;
 }
 
 
@@ -96,14 +80,20 @@ function operate(firstNumber, secondNumber, operation) {
     let displayNumberDiv = document.querySelector('.display');
     if (operation === "add") {
         result = Number(firstNumber) + Number(secondNumber);
+        result = checkDecimals(result);
+        result = checkMaxNumberReached(result);
         displayNumberDiv.innerText = result;
         firstNumber = result;
     } else if (operation === "subtract") {
         result = Number(firstNumber) - Number(secondNumber);
+        result = checkDecimals(result);
+        result = checkMaxNumberReached(result);
         displayNumberDiv.innerText = result;
         firstNumber = result;
     } else if (operation === "multiply") {
         result = Number(firstNumber) * Number(secondNumber);
+        result = checkDecimals(result);
+        result = checkMaxNumberReached(result);
         displayNumberDiv.innerText = result;
         firstNumber = result;
     } else if (operation === "divide") {
@@ -111,9 +101,24 @@ function operate(firstNumber, secondNumber, operation) {
             displayNumberDiv.innerText = "To infinity, and beyond!";
         } else {
             result = Number(firstNumber) / Number(secondNumber);
+            // Check if number is a decimal that has too many places
+            result = checkDecimals(result);
+            result = checkMaxNumberReached(result);
             displayNumberDiv.innerText = result;
             firstNumber = result;
         }
+    }
+    console.log(result);
+}
+
+
+function checkDecimals(result) {
+    let resultLength = result.toString().length;
+    if (resultLength > 9) {
+        shorterResult = Number(result.toString().slice(0, 11));
+        return shorterResult;
+    } else {
+        return result;
     }
 }
 
@@ -123,7 +128,9 @@ function equals() {
     secondNumber = getDisplayNumber();
     // Pass the global firstNumber, secondNumber and operation variables to the operate function
     operate(firstNumber, secondNumber, operation);
+    // Reset the select operation
     operation = "";
+    isOperationClicked = true;
 }
 
 
